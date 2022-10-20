a5c4293fa38be7a324fa6005ab355daab8dac29f
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-10-20 20:47:16 -0300

    modificado css de RelatedProducts

--- src/pages/Produto/RelatedProducts/RelatedProducts.jsx
+++ src/pages/Produto/RelatedProducts/RelatedProducts.jsx
@@ -1,9 +1,10 @@
 import { Produtos } from "../../../components/Produtos/Produtos";
+import "./RelatedProducts.css";
 
 export const RelatedProducts = ({ relatedProducts }) => {
   return (
     <>
-      <section className="justify-center">
+      <section className="section__relatedProducts justify-center">
         <h1>Produtos relacionados: </h1>
         <section className="section__produtos ">
           {relatedProducts !== undefined && (
